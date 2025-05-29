c53daec2750d2ede6bd01cedd9755a7b548e62ce
Rodrigo <ropin1697@gmail.com>
2025-05-29 08:42:36 -0300

    .

--- Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
+++ Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2795d666b7a2459687c4722a841eb9e98397bc1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a796210a93a01bef774ab4142bbcbaf51e9d897f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApp Obligatorio P2/obj/Debug/net8.0/WebApp Obligatorio P2.AssemblyInfo.cs
+++ WebApp Obligatorio P2/obj/Debug/net8.0/WebApp Obligatorio P2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp Obligatorio P2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2795d666b7a2459687c4722a841eb9e98397bc1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a796210a93a01bef774ab4142bbcbaf51e9d897f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp Obligatorio P2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp Obligatorio P2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
